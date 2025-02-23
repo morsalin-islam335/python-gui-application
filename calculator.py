1f7e9013615177f3d5ec77e9505c342d6898f292
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-02-23 19:11:16 +0600

    make necessary function

---
+++ calculator.py
@@ -0,0 +1,29 @@
+from tkinter import Tk, Toplevel, Frame, Label, Button, Entry, Text,Listbox,Grid, Menu, Menubutton
+expression =  ""
+
+def press(num):
+    pass
+
+def equalPress():
+    pass
+
+def clear():
+    pass
+
+
+
+def main():
+    
+    gui = Tk()# create a gui
+    gui.configure(background="light green")
+    gui.title("Calculator Using Tkniter By Morsalin Islam")
+    gui.geometry("270x160")
+
+    gui.mainloop() # it start gui
+
+    
+
+
+if __name__ == "__main__":
+    main()
+
