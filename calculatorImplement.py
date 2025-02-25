1510e22cb034d8c99fe0cb405244a19aa01fa010
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-02-25 08:37:10 +0600

    make some changes

--- calculatorImplement.py
+++ calculatorImplement.py
@@ -14,9 +14,29 @@ def main():
    entry.grid(row = 0, column = 0, columnspan = 4, padx = 40, pady = 40, sticky = "nsew")
 
    buttons = [
-       
-   ]
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', '.', '=', '+'
+]
+   
+   row_val = 1
+   col_val = 0
 
+   for button in buttons:
+       pass 
+   
+
+   tk.Button(
+       root,
+       text = "Clear",
+       padx =10,
+       bg="red",
+       fg = "black",
+       row = row_val,
+
+       column = col_val
+   )
 
    root.mainloop()
 
