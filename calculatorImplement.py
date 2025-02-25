49015cedc5eb48498188fa6e7067f8fde1cb4ce2
morsalin-islam5350 <weshallovercome335@gmail.com>
2025-02-25 23:32:37 +0600

    implement getResult function

--- calculatorImplement.py
+++ calculatorImplement.py
@@ -13,7 +13,20 @@ entry = None
 
 
 def getResult():
-    pass 
+    global expression
+    global entry
+    current = entry.get()
+    try:
+        result = eval(current)
+        entry.delete(0, tk.END) # erase all
+        entry.insert(tk.END, result)
+
+    except Exception as e:
+        entry.delete(0,tk.END)
+        entry.insert(tk.END, "Error")
+
+
+
 
 
 def buttonPress(value):
@@ -35,8 +48,68 @@ def main():
     ########## now works with entry ######
 
     entry = tk.Entry(root, font= ("Arial", 35,), justify = tk.RIGHT)
-    entry.grid(row = 0, column = 0, columnspan = 4, padx = 20, pady = 20, sticky = "nsew")
+    entry.grid(row = 0, column = 0, columnspan = 5, padx = 30, pady = 20, ipady=20,  sticky = "nsew")
     
+    buttons  = [
+        "√", "^", "(", ")",  "x²",
+        "7", "8", "9", "/", "sin",
+        "4", "5", "6", "*", "cos", 
+        "1", "2", "3", "-", "tan",
+        "0", "00", ".", "+", "sec",
+        "Clear", "Del", "="
+
+    ]
+
+
+    rowVal = 1  # 0 th row works for entry
+    colVal = 0
+
+    for button in buttons:
+        calculatorCell = tk.Button(
+            root,
+            text=button,
+            bg= "red" if (button == "Clear" or button == "Del") else "#61dafb",
+            fg="black",
+            padx= 10,
+            pady = 20,
+            font = ("Arial", 16)
+
+        )
+
+        
+        if button != "=":
+            
+            calculatorCell.grid(
+                row = rowVal,
+                column = colVal,
+                sticky = "nsew"
+                )
+        else:
+            calculatorCell.grid(
+            row = rowVal,
+            column = colVal,
+            sticky = "nsew",
+            columnspan=3
+            )
+
+
+
+
+
+        colVal += 1
+
+        if colVal == 5:
+            colVal = 0
+            rowVal += 1
+
+    
+    for i in range(6):
+        root.grid_rowconfigure(i, weight=1)
+        root.grid_columnconfigure(i, weight=1)
+
+      
+
+            
 
     root.mainloop()
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
 
-root.config(bg="#282c35")  
